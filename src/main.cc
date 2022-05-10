95c4dc54c923f8431a8552da0b766062f61e7f44
IkerGalardi <contacto.ikergalardi@gmail.com>
2022-05-10 16:25:19 +0200

    Using command line arguments to select what to display

--- src/main.cc
+++ src/main.cc
@@ -29,31 +29,78 @@ static mapped_file read_file(const char* path) {
     return mapped_file{buffer, file_size};
 }
 
+struct parser_flags {
+    bool show_header = false;
+    bool show_reservation_map = false;
+    bool show_tree_nodes = false;
+};
+
+static void print_helpfull_error_message() {
+    fprintf(stderr, "Usage: dtbparser [-hrt] file\n");
+    fprintf(stderr, "Where the arguments specify...\n");
+    fprintf(stderr, "  -h : show the header of the DTB file.\n");
+    fprintf(stderr, "  -r : show the reservation map of the DTB file\n");
+    fprintf(stderr, "  -t : show the tree nodes of the DTB file\n");
+}
+
+parser_flags get_flags(int argc, char** argv) {
+    parser_flags flags;
+
+    int option = -1;
+    while((option = getopt(argc, argv, "hrtH")) != -1) {
+        switch(option) {
+        case 'h':
+            flags.show_header = true;
+            break;
+
+        case 'r':
+            flags.show_reservation_map = true;
+            break;
+
+        case 't':
+            flags.show_tree_nodes = true;
+            break;
+
+        case 'H':
+            print_helpfull_error_message();
+            std::exit(1);
+        }
+    }
+
+    return flags;
+}
+
 int main(int argc, char** argv) {
-    auto mapped_dtb = read_file(argv[1]);
+    auto flags = get_flags(argc, argv);
+
+    auto mapped_dtb = read_file(argv[optind]);
 
     dtb::reader dtb_reader{mapped_dtb.data};
     if(!dtb_reader.ok()) {
-        printf("Something went wrong!\n");
-        printf("  · Magic number: %x\n", dtb_reader.magic());
-        printf("  · Version: %d\n", dtb_reader.version());
-        printf("  · Last compatible version: %d\n", dtb_reader.last_compatible_version());
+        fprintf(stderr, "dtbparser: File %s is not a correct DTB file.\n", argv[optind]);
         std::exit(1);
     }
 
     // Print important information about the header
-    printf("DTB Header:\n");
-    printf("  · Version: %d\n", dtb_reader.version());
-    printf("  · Last compatible version: %d\n", dtb_reader.last_compatible_version());
+    if(flags.show_header) {
+        printf("Header:\n");
+        printf("  · Magic: %d\n", dtb_reader.magic());
+        printf("  · Version: %d\n", dtb_reader.version());
+        printf("  · Last compatible version: %d\n", dtb_reader.last_compatible_version());
+    }
 
-    printf("Reserved areas:\n");
-    dtb_reader.iterate_over_reserved_areas([](const dtb::reserved_area& area) {
-        printf("  · Area:\n");
-        printf("    · Address: %x\n", area.address());
-        printf("    · Size:    %d\n", area.size());
-        fflush(stdout);
-    });
+    if(flags.show_reservation_map) {
+        printf("Reserved areas:\n");
+        dtb_reader.iterate_over_reserved_areas([](const dtb::reserved_area& area) {
+            printf("  · Area:\n");
+            printf("    · Address: %x\n", area.address());
+            printf("    · Size:    %d\n", area.size());
+            fflush(stdout);
+        });
+    }
 
-    printf("Structure Blocks\n");
-    dtb_reader.iterate_over_structure_blocks();
+    if(flags.show_tree_nodes) {
+        printf("Structure Blocks\n");
+        dtb_reader.iterate_over_structure_blocks();
+    }
 }
